37aeae695998a06028d87b0847b08e8d926443ad
DTradd12 <derektradd@gmail.com>
2020-06-12 14:01:04 -0400

    JavaScript Slackbot

---
+++ index.js
@@ -0,0 +1,7 @@
+const SlackBot = require('slackbots');
+
+// Promise based HTTP client for the browser and node.js
+const axios = require('axios');
+
+// Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
+const dotenv = require('dotenv');
